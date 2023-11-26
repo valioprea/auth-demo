00081ce862b7f6b55c125aeac17d627eb1efdfce
valioprea <vali@doordeck.com>
2023-11-26 14:21:13 +0200

    wip auth token

--- src/main/java/com/gems/security/demo/DemoController.java
+++ src/main/java/com/gems/security/demo/DemoController.java
@@ -1,8 +1,11 @@
 package com.gems.security.demo;
 
+import com.gems.security.config.JWTService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo-controller")
 public class DemoController {
 
+    private final JWTService jwtService;
+
+    public DemoController(JWTService jwtService) {
+        this.jwtService = jwtService;
+    }
+
     @GetMapping
     public ResponseEntity<String> sayHello(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authToken
     ) {
+//        var a = jwtService.extractUsername(authToken);
+//        System.out.println(a);
+        // TODO: I have problems with this. Not sure how to correctly extract the authToken
+
         return ResponseEntity.ok("hello from secured endpoint");
     }
 }
